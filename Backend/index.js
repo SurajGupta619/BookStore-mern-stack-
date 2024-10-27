42e1bb1e64330e3b77c2273f9d8de54ef2fe5ef1
Suraj Gupta <surajgupta61913@gmail.com>
2024-10-27 12:52:58 +0530

    updated

--- Backend/index.js
+++ Backend/index.js
@@ -22,7 +22,7 @@ try {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     });
-    console.log("Connected to mongoDB");
+    console.log("Connected to mongodb");
 } catch (error) {
     console.log("Error: ", error);
 }
