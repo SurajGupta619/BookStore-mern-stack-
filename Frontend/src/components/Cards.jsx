8683844cd88f995b64d360a82e52ce544a7592bf
Suraj Gupta <surajgupta61913@gmail.com>
2024-10-27 12:17:35 +0530

    complete project!

--- Frontend/src/components/Cards.jsx
+++ Frontend/src/components/Cards.jsx
@@ -6,7 +6,11 @@ function Cards({ item }) {
       <div className="mt-4 my-3 p-3">
         <div className="card w-92 bg-base-100 shadow-xl hover:scale-105 duration-200 dark:bg-slate-900 dark:text-white dark:border">
           <figure>
-            <img src={item.image} alt="Shoes" />
+            <img
+              src={item.image}
+              alt={item.name} // Updated alt text to use item.name for better accessibility
+              style={{ width: "200px", height: "300px", objectFit: "cover" }} // Fixed size and object fit
+            />
           </figure>
           <div className="card-body">
             <h2 className="card-title">
@@ -16,7 +20,7 @@ function Cards({ item }) {
             <p>{item.title}</p>
             <div className="card-actions justify-between">
               <div className="badge badge-outline">${item.price}</div>
-              <div className=" cursor-pointer px-2 py-1 rounded-full border-[2px] hover:bg-pink-500 hover:text-white duration-200">
+              <div className="cursor-pointer px-2 py-1 rounded-full border-[2px] hover:bg-pink-500 hover:text-white duration-200">
                 Buy Now
               </div>
             </div>
